c377ea87cdf2119bb4872363ee8592e8f4db58d4
miketranh <miketranh21@gmail.com>
2016-07-05 12:10:24 -0400

    change 5
    I punch through all the code because i didn't want to continue wasting
    hours trying to figure out what I did wrong. In the end i tried to run
    the code but had errors with layout. I know where the problem is because
    it was my the second time i got stuck after the layout issue of not
    being recognized. I am going back to the book and re look over the
    instructions

--- Assets/__Scripts/Card.cs
+++ Assets/__Scripts/Card.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class Card : MonoBehaviour {
-
+	
 	public string    suit;
 	public int       rank;
 	public Color     color = Color.black;
@@ -14,15 +14,62 @@ public class Card : MonoBehaviour {
 	
 	public GameObject back;  // back of card;
 	public CardDefinition def;  // from DeckXML.xml		
-
+	
+	//List of the SpriteRednderer Components of this gameObject and its children
+	public SpriteRenderer[] spriteRenderers;
+	
+	public bool faceUp {
+		get{
+			return(!back.activeSelf);
+		}
+		set{
+			back.SetActive(!value);
+		}
+	}
 	// Use this for initialization
 	void Start () {
-	
+		SetSortOrder (0);
 	}
 	
+	public void PopulateSpriteRenderers(){
+		if (spriteRenderers == null || spriteRenderers.Length == 0) {
+			spriteRenderers = GetComponentsInChildren<SpriteRenderer>();
+		}
+	}
+	
+	public void SetSortingLayerName(string tSLN){
+		PopulateSpriteRenderers ();
+		foreach (SpriteRenderer tSR in spriteRenderers) {
+			tSR.sortingLayerName = tSLN;
+		}
+	}
+	
+	public void SetSortOrder(int sOrd){
+		PopulateSpriteRenderers ();
+		
+		foreach (SpriteRenderer tSR in spriteRenderers) {
+			if(tSR.gameObject == this.gameObject){
+				tSR.sortingOrder = sOrd;
+				continue;
+			}
+			switch(tSR.gameObject.name){
+			case "back":
+				tSR.sortingOrder = sOrd + 2;
+				break;
+			case "face":
+			default:
+				tSR.sortingOrder = sOrd + 1;
+				break;
+			}
+		}
+	}
 	// Update is called once per frame
 	void Update () {
+		
+	}
 	
+	virtual public void OnMouseUpAsButton(){
+		print (name);
 	}
 } // class Card
 
@@ -39,5 +86,5 @@ public class CardDefinition{
 	public string	face;	//sprite to use for face cart
 	public int		rank;	// value from 1-13 (Ace-King)
 	public List<Decorator>	
-					pips = new List<Decorator>();  // Pips Used
+		pips = new List<Decorator>();  // Pips Used
 }
---
+++ Assets/__Scripts/CardProspector.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public enum CardState {
+	drawpile,
+	tableau,
+	target,
+	discard
+}
+public class CardProspector : Card {
+	public CardState state= CardState.drawpile;
+	public List<CardProspector> hiddenBy = new List<CardProspector>();
+	public int layoutID;
+	public SlotDef slotDef;
+
+	}
