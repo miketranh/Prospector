e8d04dbd6a27b53449487ffc886fc66088fd33ad
miketranh <miketranh21@gmail.com>
2016-07-14 09:05:23 -0400

    Bartok change 1
    did some cleaning to prospector to account for Bartok. Got the
    bartokbackground and card layout working

---
+++ Assets/__Scripts/Bartok.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Bartok : MonoBehaviour {
+	static public Bartok S;
+	public TextAsset deckXML;
+	public TextAsset layoutXML;
+	public Vector3 layoutCenter=Vector3.zero;
+	public bool _____________________;
+	public Deck deck;
+	public List<CardBartok> drawPile;
+	public List<CardBartok> discardPile;
+
+	void Awake(){
+		S = this;
+	}
+
+
+
+	// Use this for initialization
+	void Start () {
+		deck = GetComponent<Deck>();
+		deck.InitDeck(deckXML.text);
+		Deck.Shuffle(ref deck.cards);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Assets/__Scripts/BartokLayout.cs
+++ Assets/__Scripts/BartokLayout.cs
@@ -12,29 +12,28 @@ public class SlotDef{
 	public int layerID = 0;
 	public int id;
 	public List<int> hiddenBy = new List<int>();
+	public float rot;
 	public string type="slot";
 	public Vector2 stagger;
+	public int player;
+	public Vector3 pos;
 }
 
-public class Layout : MonoBehaviour {
+
+public class BartokLayout : MonoBehaviour {
 	public PT_XMLReader xmlr;
 	public PT_XMLHashtable xml;
 	public Vector2 multiplier;
-	
-	public List<SlotDef> slotDefs;
+	tableau
+		public List<SlotDef> slotDefs;
 	public SlotDef drawPile;
 	public SlotDef discardPile;
-	
-	public string[] sortingLayerNames = new string[] {
-		"Row0", "Row1", "Row2", "Row3", "Discard", "Draw"
-	};
-	
+	public SlotDef target;
 	public void ReadLayout(string xmlText){
 		xmlr = new PT_XMLReader ();
 		xmlr.Parse (xmlText);
 		xml = xmlr.xml ["xml"] [0];
-		
-		multiplier.x = float.Parse (xml ["multiplier"] [0].att ("x"));
+		multiplier.x= float.Parse(xml["multiplier"][0].att("x"))
 		multiplier.y = float.Parse (xml ["multiplier"] [0].att ("y"));
 		
 		SlotDef tSD;
@@ -49,32 +48,26 @@ public class Layout : MonoBehaviour {
 			}
 			tSD.x = float.Parse (slotsX[i].att("x"));
 			tSD.y = float.Parse (slotsX[i].att("y"));
+			tSD.pos = new Vector3(tSD.x*multiplier.x,tSD.y*multiplier.y,0)
+				//sorting Layers
 			tSD.layerID = int.Parse (slotsX[i].att("layer"));
 			
-			tSD.layerName = sortingLayerNames[tSD.layerID];
+			tSD.layerName = tSD.layerID.ToString();
 			
 			switch (tSD.type){
 			case "slot":
-				tSD.faceUp = (slotsX[i].att("faceup") == "1");
-				tSD.id = int.Parse (slotsX[i].att("id"));
-				if(slotsX[i].HasAtt("hiddenby")){
-					string[] hiding = slotsX[i].att("hiddenby").Split(',');
-					foreach (string s in hiding){
-						tSD.hiddenBy.Add (int.Parse (s));
-					}
-				}
-				slotDefs.Add (tSD);
 				break;
-				
 			case "drawpile":
-				tSD.stagger.x = float.Parse (slotsX[i].att("xstagger"));
-				drawPile = tSD;
+				s
 				break;
-				
-			case "discardpile":
-				discardPile = tSD;
-				break;
-			}
-		}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
 	}
 }
